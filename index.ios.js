70d16e933208ee905634e4f8626bce29308c6ec3
Lauren <lhspiegel@gmail.com>
2016-01-30 15:28:05 -0800

    Add Main and routing

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
+
+import Main from './App/Components/Main';
+
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View
 } from 'react-native';
-
-class fflyNative extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Natives!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111',
   },
 });
+
+class fflyNative extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+      style = {styles.container}
+      initialRoute={{
+        title: 'ffly',
+        component: Main
+      }} />
+    );
+  }
+}
 
 AppRegistry.registerComponent('fflyNative', () => fflyNative);
